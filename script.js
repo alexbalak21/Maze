9e0fbc80ecca0438dfe709201c46c9c5ba5b0a50
alexbalak21 <alex.balak@outlook.com>
2022-07-05 18:34:26 +0200

    working, no backtracking

--- script.js
+++ script.js
@@ -25,4 +25,59 @@ function drawArray(arr) {
 
 const arr = generateArray(20, 20);
 
+let x = 0;
+let y = 0;
+arr[0][0] = 0;
+
+function chooseNextBlock() {
+	//POSSIBLE DIRECTIOS ARRAY U D L R
+	const possibleDirections = [];
+	//CAN GO UP
+	if (y > 0 && arr[x][y - 1]) possibleDirections.push('u');
+
+	//CAN GO DOWN
+	if (y < 19 && arr[x][y + 1]) possibleDirections.push('d');
+
+	//CAN GO LEFT
+	if (x > 0 && arr[x - 1][y]) possibleDirections.push('l');
+
+	//CAN GO RIGHT
+	if (x < 19 && arr[x + 1][y]) possibleDirections.push('r');
+
+	if (possibleDirections.length == 0) return 0;
+
+	let i = Math.round(Math.random() * (possibleDirections.length - 1));
+
+	console.log(possibleDirections);
+	console.log('Go ', possibleDirections[i]);
+
+	if (possibleDirections[i] == 'u') {
+		y--;
+		arr[x][y] = 0;
+		return 1;
+	}
+	if (possibleDirections[i] == 'd') {
+		y++;
+		arr[x][y] = 0;
+		return 2;
+	}
+	if (possibleDirections[i] == 'l') {
+		x--;
+		arr[x][y] = 0;
+		return 3;
+	}
+	if (possibleDirections[i] == 'r') {
+		x++;
+		arr[x][y] = 0;
+		return 4;
+	}
+}
+let res = 0;
+let i = 0;
+do {
+	res = chooseNextBlock();
+	i++;
+} while (res != 0);
+
 drawArray(arr);
+console.log(i);
